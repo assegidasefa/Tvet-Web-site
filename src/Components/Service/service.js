7f6bb9633d633c76eb0c676161d9e10c58f6826f
assegidasefa <assegidassefa7@gmail.com>
2023-02-07 10:27:38 +0300

    styling service

--- src/Components/Service/service.js
+++ src/Components/Service/service.js
@@ -35,15 +35,15 @@ const Service = () => {
   return (
 
 
-    <div name="service" className="w-full h-80 flex justify-around">
+    <div name="service" className="w-full h-80 flex justify-around mb-5">
     
       <div className="w-56  flex flex-col items-center bg-gray-500 rounded-xl">
-        <h1 className="text-2xl">Theory</h1>
+        <h1 className="text-2xl text-white pt-3">Theory</h1>
         <div
           className="w-32 flex justify-center items-center"
           ref={theory}
         ></div>
-        <h1>
+        <h1 className="text-white">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s,
@@ -51,12 +51,12 @@ const Service = () => {
       </div>
 
       <div className="w-56 bg-green-200 flex flex-col items-center bg-gray-500 rounded-xl">
-        <h1 className="text-2xl">Practice</h1>
+        <h1 className="text-2xl text-white pt-3">Practice</h1>
         <div
-          className="w-32 flex justify-center items-center"
+          className="w-32 flex justify-center items-center py-5"
           ref={practice}
           ></div>
-        <h1>
+        <h1 className="text-white">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s,
@@ -64,12 +64,12 @@ const Service = () => {
       </div>
 
       <div className="w-56 bg-green-200 flex flex-col items-center bg-gray-500 rounded-xl">
-        <h1 className="text-2xl">Life Skill</h1>
+        <h1 className="text-2xl text-white pt-3">Life Skill</h1>
         <div
-          className="w-32 flex justify-center items-center"
+          className="w-32 flex justify-center items-center py-5"
           ref={lifeSkill}
         ></div>
-        <h1>
+        <h1 className="text-white">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s,
