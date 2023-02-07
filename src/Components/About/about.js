ee799c91520dc756f34cf8c53202af47e390ccc2
assegidasefa <assegidassefa7@gmail.com>
2023-02-07 10:21:52 +0300

    adding style about page

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -4,8 +4,8 @@ const about = () => {
   return (
     <div name="about" className="w-full h-96 flex justify-between p-2">
       <div className="flex w-1/2 flex-col">
-        <h1 className="text-4xl pb-3 font-bold">Title</h1>
-        <h1 className="text-md p-10 font-mono text-center">
+        <h1 className="text-4xl pb-3 font-bold text-white">Title</h1>
+        <h1 className="text-md p-10 font-mono text-center text-white">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, Lorem Ipsum is simply dummy text of the printing and
@@ -14,8 +14,8 @@ const about = () => {
         </h1>
       </div>
       <div className="flex w-1/2 flex-col">
-        <h1 className="text-4xl font-bold">Title</h1>
-        <h1 className="text-md p-10 font-mono text-center">
+        <h1 className="text-4xl font-bold text-white">Title</h1>
+        <h1 className="text-md p-10 font-mono text-center text-white">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, Lorem Ipsum is simply dummy text of the printing and
